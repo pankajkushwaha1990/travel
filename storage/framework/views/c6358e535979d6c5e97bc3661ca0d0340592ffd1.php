fa46f5d45748a7193f9e5d9970e671ba17f630ca
pankajkushwaha1990 <pankajkushwaha1990@gmail.com>
2020-05-06 17:42:01 +0530

    file adde

--- storage/framework/views/c6358e535979d6c5e97bc3661ca0d0340592ffd1.php
+++
@@ -1,8 +0,0 @@
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Help
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; <?php echo e(date('Y')); ?> <a href="#">Ashton</a>.</strong> All rights reserved.
-  </footer>
